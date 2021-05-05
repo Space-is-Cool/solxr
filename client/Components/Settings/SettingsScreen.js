a4d6cf324f84928b8403f22c38cb61e76e5d9367
Arin <adavisfilm@gmail.com>
2021-05-05 08:42:50 -0500

    working on favorites

--- client/Components/Settings/SettingsScreen.js
+++ client/Components/Settings/SettingsScreen.js
@@ -1,70 +1,77 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import { Button, Settings, StyleSheet, Text, View, Switch, Alert } from "react-native";
+import React, { useState } from 'react';
+import { Button, Settings, StyleSheet, Text, View, Switch, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import LinearGradient from 'react-native-linear-gradient';
-import AwesomeButton from "react-native-really-awesome-button";
+import AwesomeButton from 'react-native-really-awesome-button';
 
 
-const Settings = () => {
+const SettingsScreen = () => {
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
-const FourthScreen = ({navigation, route}) => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const [isEnabled2, setIsEnabled2] = useState(false);
-  const [isEnabled3, setIsEnabled3] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-  const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
-  const toggleSwitch3 = () => setIsEnabled3(previousState => !previousState);
+  const FourthScreen = ({navigation, route}) => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const [isEnabled2, setIsEnabled2] = useState(false);
+    const [isEnabled3, setIsEnabled3] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
+    const toggleSwitch3 = () => setIsEnabled3(previousState => !previousState);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.value}>Settings</Text>
-      <Text style={styles.value}>Readable Font</Text>
-      <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-      <Text style={styles.value}>Music</Text>
-      <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isEnabled2 ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch2}
-        value={isEnabled2}
-      />
-      <Text style={styles.value}>NASA Theme</Text>
-      <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isEnabled3 ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch3}
-        value={isEnabled3}
-      />
-  <AwesomeButton
-      style={styles.button}
-      progress
-      onPress={next => {
-        /** Do Something **/
-        next();
-      }}
-    >
+    return (
+      <View style={styles.container}>
+        <Text style={styles.value}>Settings</Text>
+        <Text style={styles.value}>Readable Font</Text>
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isEnabled
+            ? '#f5dd4b'
+            : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+        <Text style={styles.value}>Music</Text>
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isEnabled2
+            ? '#f5dd4b'
+            : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch2}
+          value={isEnabled2}
+        />
+        <Text style={styles.value}>NASA Theme</Text>
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isEnabled3
+            ? '#f5dd4b'
+            : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch3}
+          value={isEnabled3}
+        />
+        <AwesomeButton
+          style={styles.button}
+          progress
+          onPress={next => {
+            /** Do Something **/
+            next();
+          }}
+        >
       Log Out
-    </AwesomeButton>
-    </View>
-  );
-}
+        </AwesomeButton>
+      </View>
+    );
+  };
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   value: {
     fontSize: 24,
@@ -75,13 +82,5 @@ const styles = StyleSheet.create({
   }
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
 
-export default Settings;
-
+export default SettingsScreen;
