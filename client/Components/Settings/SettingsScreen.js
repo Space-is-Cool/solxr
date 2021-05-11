9a99108f8567c2a6be0368904498d2fc6dc6efcd
Arin <adavisfilm@gmail.com>
2021-05-10 20:19:39 -0500

    minor tweaks

--- client/Components/Settings/SettingsScreen.js
+++ client/Components/Settings/SettingsScreen.js
@@ -1,33 +1,31 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import AwesomeButton from 'react-native-really-awesome-button';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { MusicContext, SoundOneContext } from '../Root/Context';
 import axios from 'axios';
+import { Switch } from 'react-native-switch';
 import Sound from 'react-native-sound';
 import LoginModal from '../Root/Login.js';
 import {sound1} from '../Root/soundOne.js';
-
-import { Switch } from 'react-native-switch';
-
 const SettingsScreen = ({navigation, route}) => {
   const [toggle, setToggle] = useState({
     accessibility: false,
     music: true,
-    theme: false
+    theme: false,
+    email: false
   });
-
+  const [emailInput, setEmailInput] = useState('');
   useEffect(() => {
     getUser();
   }, []);
-
   const getUser = async () => {
     try {
       const storedUser = await AsyncStorage.getItem('user');
-      const {accessibility, music, theme} = JSON.parse(storedUser);
-      setToggle({accessibility, music, theme});
+      const {accessibility, music, theme, email} = JSON.parse(storedUser);
+      setToggle({accessibility, music, theme, email});
     } catch (e) {
       console.log('error', e);
     }
@@ -39,10 +37,7 @@ const SettingsScreen = ({navigation, route}) => {
     } else {
       sound1.play();
     }
-  
   };
-
-
   const modUser = async (prop) => {
     try {
       const storedUser = await AsyncStorage.getItem('user');
@@ -58,16 +53,38 @@ const SettingsScreen = ({navigation, route}) => {
       console.log('there was an error', e);
     }
   };
-
+  const modEmail = async () => {
+    try {
+      const storedUser = await AsyncStorage.getItem('user');
+      const user = JSON.parse(storedUser);
+      user.email = emailInput;
+      await AsyncStorage.setItem('user', JSON.stringify(user));
+      setToggle(() => {
+        const copy = Object.assign({}, toggle);
+        copy.email = !copy.email;
+        return copy;
+      });
+      setEmailInput('');
+      saveToServer();
+    } catch (e) {
+      console.log('there was an error', e);
+    }
+  };
   const clearStorage = async () => {
     try {
       await AsyncStorage.removeItem('user');
     } catch (e) {
-      console.log(e);
+      console.info(e);
     }
     console.log('Done.');
   };
-
+  // const saveEmail = async () => {
+  //   const storedUser = await AsyncStorage.getItem('user');
+  //   const user = JSON.parse(storedUser);
+  //   axios.put('http://ec2-3-134-108-148.us-east-2.compute.amazonaws.com:3001/users/update', user)
+  //     .then(() => console.log('success!!!'))
+  //     .catch(err => console.log('fail', err));
+  // };
   const saveToServer = async () => {
     const storedUser = await AsyncStorage.getItem('user');
     const user = JSON.parse(storedUser);
@@ -75,7 +92,6 @@ const SettingsScreen = ({navigation, route}) => {
       .then(() => console.log('success!!!'))
       .catch(err => console.log('fail', err));
   };
-
   return (
     <MusicContext.Consumer>{ ({ music, setMusic }) => (
       <View style={styles.container}>
@@ -104,8 +120,8 @@ const SettingsScreen = ({navigation, route}) => {
           onValueChange={() => { setMusic(!music); musicToggle(music); modUser('music'); }}
           value={toggle.music}
         />
-        <Text style={styles.value}>NASA Theme</Text>
-        <Switch
+        {/* <Text style={styles.value}>NASA Theme</Text> */}
+        {/* <Switch
           style={styles.switch}
           circleActiveColor={'#9ee7ff'}
           circleInActiveColor={'#f4f3f4'}
@@ -115,9 +131,37 @@ const SettingsScreen = ({navigation, route}) => {
           switchRightPx={5} 
           onValueChange={() => modUser('theme')}
           value={toggle.theme}
-        />
+        /> */}
+        <Text style={styles.value}>Sign up for Astral Emails:</Text>
+        {toggle.email ?
+          <AwesomeButton
+            style={styles.button}
+            width={200}
+            height={50}
+            onPress={modEmail}>
+            Unsubscribe
+          </AwesomeButton> :
+          <>
+            <TextInput
+              style={styles.input}
+              onChangeText={setEmailInput}
+              value={emailInput}/>
+            <AwesomeButton
+              style={styles.button}
+              width={200}
+              height={50}
+              backgroundColor={emailInput
+                ? 'rgb(7, 40, 82)'
+                : '#C0C0C0'}	
+              onPress={modEmail}
+            >Submit
+            </AwesomeButton>
+          </>
+        }
         <AwesomeButton
           style={styles.button}
+          width={200}
+          height={50}
           // progress
           onPress={saveToServer}
         >
@@ -125,6 +169,8 @@ const SettingsScreen = ({navigation, route}) => {
         </AwesomeButton>
         <AwesomeButton
           style={styles.button}
+          width={200}
+          height={50}
           progress
           onPress={() => {
             clearStorage();
@@ -138,7 +184,6 @@ const SettingsScreen = ({navigation, route}) => {
     }</MusicContext.Consumer>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -150,8 +195,16 @@ const styles = StyleSheet.create({
     marginVertical: 12
   },
   button: {
-    marginTop: '20%'
+    marginBottom: '10%',
+  },
+  switch: {
+    marginBottom: '30%',
+  },
+  input: {
+    height: 40,
+    width: '50%',
+    margin: 12,
+    borderWidth: 1,
   }
 });
-
 export default SettingsScreen;
