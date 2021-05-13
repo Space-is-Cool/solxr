f80c4ef7b0bf4dbf40c2fbbd857ec664f8d03627
samjohnson097 <samjohnson097@gmail.com>
2021-05-13 16:43:03 -0500

    portal basics working

--- client/Components/Experience/PortalAR.js
+++ client/Components/Experience/PortalAR.js
@@ -9,6 +9,7 @@ import React, { Component } from 'react';
 import {
   ViroSceneNavigator,
   ViroScene,
+  ViroMaterials,
   ViroARScene,
   ViroAmbientLight,
   Viro360Video,
@@ -30,15 +31,28 @@ const Portal = ({navigation, route}) => {
       <ViroARScene>
         <ViroAmbientLight color="#ffffff" intensity={200} />
         <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={() => {}}>
-          <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]}>
-            <Viro3DObject source={require('./assets/pumpkin_tall_10k.obj')}
-              type="jpeg" />
+          <ViroPortal position={[0, 0, -1]} scale={[1, 1, 1]} opacity={0.5}>
+            {/* <Viro3DObject source={require('./assets/pumpkin_tall_10k.obj')}
+              type="OBJ" /> */}
+            <Viro3DObject source={require('./assets/heart.obj')}
+              materials={['heart']} type="OBJ" />
           </ViroPortal>
           <Viro360Image source={require('./assets/milky_way.jpeg')} />
         </ViroPortalScene>
       </ViroARScene>
     );
   };
+
+  ViroMaterials.createMaterials({
+    heart: {
+      lightingModel: 'Blinn',
+      diffuseTexture: require('./assets/Heart_D3.jpeg'),
+      specularTexture: require('./assets/Heart_S2.jpeg'),
+      writesToDepthBuffer: true,
+      readsFromDepthBuffer: true,
+      // opacity: [0, 0, 0]
+    },
+  });
 
   return (
     <>
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,14 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+// module.exports = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const {
+    resolver: { assetExts },
+  } = await getDefaultConfig();
+  return {
+    resolver: {
+      assetExts: [...assetExts, 'obj', 'mtl', 'JPG', 'jpeg', 'vrx', 'fbx', 'gltf', 'glb', 'hdr'],
+    },
+  };
+})();
