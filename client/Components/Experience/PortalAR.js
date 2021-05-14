7753cc685876fa034990ccc084e9108e2c7d5713
samjohnson097 <samjohnson097@gmail.com>
2021-05-14 12:29:54 -0500

    AR portal working

--- client/Components/Experience/PortalAR.js
+++ client/Components/Experience/PortalAR.js
@@ -31,13 +31,48 @@ const Portal = ({navigation, route}) => {
       <ViroARScene>
         <ViroAmbientLight color="#ffffff" intensity={200} />
         <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={() => {}}>
-          <ViroPortal position={[0, 0, -1]} scale={[1, 1, 1]} opacity={0.5}>
-            {/* <Viro3DObject source={require('./assets/pumpkin_tall_10k.obj')}
-              type="OBJ" /> */}
-            <Viro3DObject source={require('./assets/heart.obj')}
-              materials={['heart']} type="OBJ" />
+          {/* front view */}
+          <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]} opacity={1}>
+            <Viro3DObject source={require('./assets/portal_ship.vrx')}
+              resources={[require('./assets/portal_ship_diffuse.png'),
+                require('./assets/portal_ship_normal.png'),
+                require('./assets/portal_ship_specular.png')]}
+              type="VRX"/>
+          </ViroPortal>
+          <Viro360Image source={require('./assets/atacoma_desert4k.tiff')} />
+        </ViroPortalScene>
+        <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={() => {}}>
+          {/* back view */}
+          <ViroPortal position={[0, 0, 1]} scale={[.1, .1, .1]} rotation={[0, 180, 0]} opacity={1}>
+            <Viro3DObject source={require('./assets/portal_ship.vrx')}
+              resources={[require('./assets/portal_ship_diffuse.png'),
+                require('./assets/portal_ship_normal.png'),
+                require('./assets/portal_ship_specular.png')]}
+              type="VRX"/>
           </ViroPortal>
           <Viro360Image source={require('./assets/milky_way.jpeg')} />
+        </ViroPortalScene>
+        <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={() => {}}>
+          <ViroPortal position={[1, 0, 0]} scale={[.1, .1, .1]} rotation={[0, 270, 0]} opacity={1}>
+            {/* right hand side view */}
+            <Viro3DObject source={require('./assets/portal_ship.vrx')}
+              resources={[require('./assets/portal_ship_diffuse.png'),
+                require('./assets/portal_ship_normal.png'),
+                require('./assets/portal_ship_specular.png')]}
+              type="VRX"/>
+          </ViroPortal>
+          <Viro360Image source={require('./assets/space_shuttle360.png')} />
+        </ViroPortalScene>
+        <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={() => {}}>
+          {/* left hand side view */}
+          <ViroPortal position={[-1, 0, 0]} scale={[.1, .1, .1]} rotation={[0, 90, 0]} opacity={1}>
+            <Viro3DObject source={require('./assets/portal_ship.vrx')}
+              resources={[require('./assets/portal_ship_diffuse.png'),
+                require('./assets/portal_ship_normal.png'),
+                require('./assets/portal_ship_specular.png')]}
+              type="VRX"/>
+          </ViroPortal>
+          <Viro360Image source={require('./assets/earth_moon.jpeg')} />
         </ViroPortalScene>
       </ViroARScene>
     );
--- metro.config.js
+++ metro.config.js
@@ -8,7 +8,7 @@ module.exports = (async () => {
   } = await getDefaultConfig();
   return {
     resolver: {
-      assetExts: [...assetExts, 'obj', 'mtl', 'JPG', 'jpeg', 'vrx', 'fbx', 'gltf', 'glb', 'hdr'],
+      assetExts: [...assetExts, 'obj', 'mtl', 'JPG', 'jpeg', 'vrx', 'fbx', 'gltf', 'glb', 'hdr', 'tiff'],
     },
   };
 })();
