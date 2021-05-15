98aa019188c10b25223a8a2aa1924162f3a679e6
Bradley Black <bradleyblack@Bradleys-MacBook-Air.local>
2021-05-15 16:43:48 -0500

    completed splash page, email sign up feature

--- server/routes/email.js
+++ server/routes/email.js
@@ -18,17 +18,15 @@ const emailBlast = (credA, credB) => {
     }
   });
 
-  const weeklyEmail = new CronJob('0 9 * * sun', async () => {
+  const weeklyEmail = new CronJob('*/5 * * * *', async () => {
 
     const userList = await User.findAll({
-      // attributes: ['email'],
       where: {
         email: {
           [Op.ne]: null
         }
       }
     }).then((data) => data.map(user => user.dataValues.email));
-    
     const emailBody = (events) => {
       const weekStarts = moment().format('YYYYMMDD');
       const weekEnds = moment().add(1, 'week').format('YYYYMMDD');
@@ -38,10 +36,9 @@ const emailBlast = (credA, credB) => {
       }).map(({summary, description, uid}) => {
         const eventDate = uid.slice(0, 8);
         const formatDate = [eventDate.slice(4, 6), eventDate.slice(6), eventDate.slice(0, 4)].join('-');
-        // return summary + '\n' + formatDate + '\n' + description + '\n\n';
         return [summary, formatDate, description].join('\n');
     
-      }).join('\n\n')
+      }).join('\n\n').concat('\n\n Unsubscribe \n\n http://ec2-3-134-108-148.us-east-2.compute.amazonaws.com:3001/unsubscribe')
         .toString();
     };
 
